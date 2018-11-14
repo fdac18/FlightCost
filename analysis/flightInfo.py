74d6ca3b4c7e5e5a70bf04ac57e791fb1992618b
zol0 <akarnauc@vols.utk.edu>
2018-11-14 19:36:20 +0000

    Made auto detecting airport version of Cais function

--- analysis/flightInfo.py
+++ analysis/flightInfo.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python
+from geopy import geocoders, distance
+import subprocess
+import googlemaps
+from GeoBases import GeoBase
 import MySQLdb
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def extractDataODQ():
-    db = MySQLdb.connect(host="10.16.0.3",    # your host, usually localhost
-            user="root",         # your username
-            passwd="flight-cost18",  # your password
-            db="FlightData")        # name of the data base
+GMAPS_KEY = 'AIzaSyCC7J4WeMBCJiwoEPUhm9-mOZlc8NDR7Kc'
+MILE_RADIUS = 160
+def connect():
+    db = MySQLdb.connect(host="10.16.0.3",          # your host, usually localhost
+                         user="root",               # your username
+                         passwd="flight-cost18",    # your password
+                         db="FlightData")           # name of the data base
 
+    return db
+
+def airportsNearCoords(location, radius):
+    g = geocoders.GoogleV3(api_key=GMAPS_KEY)
+    geo_a = GeoBase(data='airports', verbose=False)
+
+    # Build list of primary airports for filtering IATA coded airports
+    f = open('airports.txt', 'r')
+    IATA = [line.strip() for line in f.readlines()]
+    f.close()
+
+    # Lookup airports near a destination
+    address0, (lat0, long0) = g.geocode(location, exactly_one=False)[0]
+    latlong = (lat0, long0)
+    near = sorted(geo_a.findNearPoint((lat0, long0), radius))
+    airports = [k for _, k in near if k in IATA]
+    return airports
+
+def extractDataODQ():
+    db = connect()
     cur = db.cursor()
 
     origin = raw_input('input origin airport: ')
@@ -28,18 +56,14 @@ def extractDataODQ():
 
 # Second function with arguments passed
 def extractPassPriceODQ(org, des, quart):
-    db = MySQLdb.connect(host="10.16.0.3",    # your host, usually localhost
-            user="root",         # your username
-            passwd="flight-cost18",  # your password
-            db="FlightData")        # name of the data base
-
+    db = connect()
     cur = db.cursor()
 
     origin = org
     dest = des
     quarter = quart
 
-    command = 'Select mktFare FROM HistoricalData WHERE Origin = "' + origin + '" AND Dest = "' + dest + '" AND Quarter = "' + quarter + '" AND Passengers = "1"'
+    command = 'Select mktFare FROM HistoricalData WHERE Origin = "' + origin + '" AND Dest = "' + dest + '" AND Quarter = "' + str(quarter) + '" AND Passengers = "1"'
 
     cur.execute(command)
     result1 = cur.fetchall()
@@ -48,11 +72,7 @@ def extractPassPriceODQ(org, des, quart):
     return result1
 
 def extractDataOD():
-    db = MySQLdb.connect(host="10.16.0.3",    # your host, usually localhost
-            user="root",         # your username
-            passwd="flight-cost18",  # your password
-            db="FlightData")        # name of the data base
-
+    db = connect()
     cur = db.cursor()
 
     origin = raw_input('Input origin airport: ')
@@ -105,3 +125,36 @@ def yearlyTrend():
     plt.xlabel("Fiscal Quarter")
     plt.xticks(range(1,5), ('Jan-Mar', 'Apr-Jun', 'Jul-Sep', 'Oct-Dec'))
     plt.savefig("./yearlyTrend_"+origin+"_"+dest+".pdf")
+
+
+def yearlyTrendAuto():
+
+    start = raw_input('Enter your location: ')
+    radius = raw_input('Search for airports within how many miles of your location?: ')
+    dest = raw_input('Input destination airport: ')
+
+    originAirports = airportsNearCoords(start, radius)
+    print "Got airports " + str(originAirports)
+
+    
+    plt.style.use('ggplot')
+    plt.suptitle("Yearly Trend of Flight Cost Per Passenger")
+    plt.title(start + ' to ' + dest)
+    plt.ylabel("Cost in USD ($)")
+    plt.xlabel("Fiscal Quarter")
+    plt.xticks(range(1,5), ('Jan-Mar', 'Apr-Jun', 'Jul-Sep', 'Oct-Dec'))
+    start = start.replace(" ", "")
+    fname = "yearlyTrendAuto_" + start + "_" + dest + ".pdf"
+
+    for origin in originAirports:
+        cost=[]
+        for i in range(1,5):
+            cost.append(avgCostperQinternal(origin, dest, i))
+        plt.plot(range(1, 5), cost, label=origin)
+
+    plt.legend()
+    plt.savefig(fname)
+    print "Attempting to move the file into Google Storage Bucket\n"
+    command = "gsutil cp " + fname + " gs://graph_data"
+    command = command.split()
+    move_file = subprocess.call(command)
